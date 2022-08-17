984f734b6bea6307c81a0c9c88b0928c3803e321
Rakib-Ahmed01 <rakibahmed34360@gmail.com>
2022-08-17 12:19:22 +0600

    login page done

--- login.js
+++ login.js
@@ -3,23 +3,6 @@ const email = form["email"];
 const password = form["password"];
 const submitBtn = document.getElementById("submit-btn");
 
-// function validate() {
-//   if (email.value === "manush@gmail.com" && password.value === "12345") {
-//     window.location.href = "http://127.0.0.1:5500/banking.html";
-//   } else {
-//     alert("Enter Valid Email & Password.");
-//   }
-// }
-
-// form.addEventListener("submit", () => {
-//   console.log(email.value);
-//   console.log(password.value);
-//   if (email.value === "manush@gmail.com" || password.value === "12345") {
-//     window.location.href = "../banking.html";
-//   } else {
-//     alert("Enter Valid Email & Password.");
-//   }
-// });
 submitBtn.addEventListener("click", () => {
   console.log(email.value);
   console.log(password.value);
